3ff33da47a962772cef49070c5d0aa9316bc1a18
MarkBeresford <mberesford301@gmail.com>
2017-02-22 10:43:48 +0000

    QC-154-JUnitSeatTypeEntity
    Added scr/test/java folder, moved JUnit tests into that folder.

--- src/test/java/come/qa/cinema/entityTest/SeatTypeJUnitEntity.java
+++ src/test/java/come/qa/cinema/entityTest/SeatTypeJUnitEntity.java
@@ -1,14 +1,16 @@
-package com.qa.cinema.persistence;
+package come.qa.cinema.entityTest;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.qa.cinema.persistence.SeatType;
+
 public class SeatTypeJUnitEntity {
 
 	@Test
 	public void testSeatTypeId() {
-
+ 
 		SeatType seatType = new SeatType();
 		seatType.setSeatTypeId(4);
 		assertEquals(seatType.getSeatTypeId(), 4);
