fc56bade1cd56dc6bd7eddb13ff8e7131cf102ba
Duncan McRobert <dmcrobert@live.co.uk>
2017-02-22 10:08:51 +0000

    TicketEntityTestQC-156
    Added Ticket Entity Tests

--- src/main/java/com/qa/cinema/persistence/TicketEntityTest.java
+++ src/main/java/com/qa/cinema/persistence/TicketEntityTest.java
@@ -7,8 +7,36 @@ import org.junit.Test;
 public class TicketEntityTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testTicket() {
+		assertNotNull(new Ticket());
+		assertNotNull(new Ticket(0, 0, "adult", 0));
+	}
+	
+	@Test
+	public void testGetTicketId() {
+		Ticket ticket = new Ticket();
+		ticket.setTicketId(5);
+		assertEquals(5, ticket.getTicketId());
+	}
+	
+	@Test
+	public void testGetBookingId() {
+		Ticket ticket = new Ticker();
+		ticket.setBookingId(5);
+		assertEquals(5, ticket.getBookingId);
 	}
 
+	@Test
+	public void testGetTicketType() {
+		Ticket ticket = new Ticket();
+		ticket.setType("child");
+		assertEquals("child", ticket.getType());
+	}
+	
+	@Test
+	public void ticketGetSeatId() {
+		Ticket ticket = new Ticket();
+		ticket.setSeatId(5);
+		assertEquals(5, ticket.getSeatId());
+	}
 }
