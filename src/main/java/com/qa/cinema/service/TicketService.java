79b47a54d569b2157733774d2630875d1c704b1d
Duncan McRobert <dmcrobert@live.co.uk>
2017-02-22 16:43:42 +0000

    QC-163TestForTicketRestfulService
    Initial Commit with interface and db implementation of service and
    method stubs. In preperation for mockito tests.

---
+++ src/main/java/com/qa/cinema/service/TicketService.java
@@ -0,0 +1,10 @@
+package com.qa.cinema.service;
+
+public interface TicketService {
+	
+	String getAllTickets();
+	
+	String getTicketByTicketID();
+	
+	String getAllTicketsByBookingID();
+}
---
+++ src/main/java/com/qa/cinema/service/TicketServiceDBImpl.java
@@ -0,0 +1,23 @@
+package com.qa.cinema.service;
+
+public class TicketServiceDBImpl implements TicketService {
+
+	@Override
+	public String getAllTickets() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getTicketByTicketID() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getAllTicketsByBookingID() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
