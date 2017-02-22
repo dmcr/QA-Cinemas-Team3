a33660bec4d65edf05d15216261f711cb8a3c0ea
davidJacob93 <david_524@hotmail.co.uk>
2017-02-22 11:15:14 +0000

    final final commit

--- src/test/java/com/qa/cinema/entityTest/ScreenTest.java
+++ src/test/java/com/qa/cinema/entityTest/ScreenTest.java
@@ -8,41 +8,41 @@ public class ScreenTest {
 
 	@Test
 	public void getScreenIDtest() {
-		Screen a = new Screen(12,34,100);
-		assertEquals(12,a.getScreenId());
+		Screen screen = new Screen(12,34,100);
+		assertEquals(12,screen.getScreenId());
 	}
 	
 	@Test
 	public void getScreentypeIDtest() {
-		Screen a = new Screen(12,34,100);
-		assertEquals(34,a.getScreenTypeId());
+		Screen screen = new Screen(12,34,100);
+		assertEquals(34,screen.getScreenTypeId());
 	}
 	
 	@Test
 	public void getScreencapacitytest() {
-		Screen a = new Screen(12,34,100);
-		assertEquals(100,a.getScreencapacity());
+		Screen screen = new Screen(12,34,100);
+		assertEquals(100,screen.getScreencapacity());
 	}
 
 	@Test
 	public void setscreenIDTest() {
-		Screen a = new Screen(12,34,100);
-		a.setScreenId(12345);
-		assertEquals(12345,a.getScreenId());
+		Screen screen = new Screen(12,34,100);
+		screen.setScreenId(12345);
+		assertEquals(12345,screen.getScreenId());
 	}
 	
 	@Test
 	public void setscreentypeIDTest() {
-		Screen a = new Screen(12,34,100);
-		a.setScreenTypeId(45);
-		assertEquals(45,a.getScreenTypeId());
+		Screen screen = new Screen(12,34,100);
+		screen.setScreenTypeId(45);
+		assertEquals(45,screen.getScreenTypeId());
 	}
 	
 	@Test
 	public void setscreencapacityTest() {
-		Screen a = new Screen(12,34,100);
-		a.setScreencapacity(200);
-		assertEquals(200,a.getScreencapacity());
+		Screen screen = new Screen(12,34,100);
+		screen.setScreencapacity(200);
+		assertEquals(200,screen.getScreencapacity());
 	}
 	
 }
