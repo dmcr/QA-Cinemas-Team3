a806f7e48e28dd3d7ddc769b2bf082b60e32586c
Duncan McRobert <dmcrobert@live.co.uk>
2017-02-22 09:26:27 +0000

    TicketEntityTest QC-156
    Initial Commit

---
+++ src/main/java/com/qa/cinema/persistence/TicketEntityTest.java
@@ -0,0 +1,14 @@
+package com.qa.cinema.persistence;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TicketEntityTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
