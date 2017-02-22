2a11683d6803c3fd43a21983a66bc1b0e800df42
MarkBeresford <mberesford301@gmail.com>
2017-02-22 11:01:44 +0000

    Edited Junit tests for SeatType.

--- src/test/java/come/qa/cinema/entityTest/SeatTypeJUnitEntity.java
+++ src/test/java/come/qa/cinema/entityTest/SeatTypeJUnitEntity.java
@@ -9,31 +9,33 @@ import com.qa.cinema.persistence.SeatType;
 public class SeatTypeJUnitEntity {
 
 	@Test
-	public void testSeatTypeId() {
+	public void testGetSeatTypeId() {
  
 		SeatType seatType = new SeatType();
 		seatType.setSeatTypeId(4);
-		assertEquals(seatType.getSeatTypeId(), 4);
+		assertEquals(4, seatType.getSeatTypeId());
 	
 	}
 	
 	@Test
-	public void testSeatTypePrice() {
+	public void testGetSeatTypePrice() {
 
 		SeatType seatType = new SeatType();
 		seatType.setSeatTypePrice(10.00);
-		assertEquals(seatType.getSeatTypePrice(), 10.00);
+		assertEquals(10.00, seatType.getSeatTypePrice());
 	
 	}
 	
 	@Test
-	public void testSeatTypeName() {
+	public void testGetSeatTypeName() {
 
 		SeatType seatType = new SeatType();
 		seatType.setSeatTypeName("child");
-		assertEquals(seatType.getSeatTypeName(), "child");
+		assertEquals("child", seatType.getSeatTypeName());
 	
 	}
+	
+	
 	
 	
 
