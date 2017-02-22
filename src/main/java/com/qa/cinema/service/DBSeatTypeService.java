31b2adfa26bea806e14ea5d818996e1ff776f9b8
MarkBeresford <mberesford301@gmail.com>
2017-02-22 16:42:04 +0000

    ..

--- src/main/java/com/qa/cinema/service/DBSeatTypeService.java
+++ src/main/java/com/qa/cinema/service/DBSeatTypeService.java
@@ -1,9 +1,26 @@
 package com.qa.cinema.service;
 
+import java.util.Collection;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import com.qa.cinema.util.JSONUtil;
+
 public class DBSeatTypeService implements SeatTypeService{
 
+	@PersistenceContext(unitName = "primary")
+	private EntityManager em;
+
+	@Inject
+	private JSONUtil util;
+	
 	public String listAllSeatTypes(){	
-		return null;
+		Query query = em.createQuery("SELECT m FROM Movie m");
+		Collection<SeatType> movies = (Collection<Movie>) query.getResultList();
+		return util.getJSONForObject(movies);
 	};
 	
 	
