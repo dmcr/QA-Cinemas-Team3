83aea602a80794a63c24d71e64c9a06857ad01a2
davidJacob93 <david_524@hotmail.co.uk>
2017-02-23 15:16:51 +0000

    final commit for davids import

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -11,4 +11,4 @@ insert into cin_seat(seat_number, seat_row, seat_type_id, seat_screen_id) values
 insert into cin_seat(seat_number, seat_row, seat_type_id, seat_screen_id) values
 (11, "A", 2, 1);
 
-INSERT INTO Screen (screen_type_id,cin_screen_capacity) VALUES ('2','100');
+INSERT INTO cin_screen (screen_type_id,cin_screen_capacity) VALUES ('2','100');
