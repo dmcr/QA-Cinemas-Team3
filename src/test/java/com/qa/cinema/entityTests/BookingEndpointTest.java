1c563d4149952d61b9f44a88556b0d32fb74239b
YoureAllDeadToMe <jackjack.iot@gmail.com>
2017-02-23 09:52:39 +0000

    Removed and updated Endpoint files

--- src/test/java/com/qa/cinema/entityTests/BookingEndpointTest.java
+++ src/test/java/com/qa/cinema/entityTests/BookingEndpointTest.java
@@ -2,6 +2,7 @@ package com.qa.cinema.entityTests;
 
 import static org.junit.Assert.fail;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -11,6 +12,7 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import com.qa.cinema.rest.BookingEndpoint;
 import com.qa.cinema.service.BookingService;
+import com.qa.cinema.service.DBSeatService;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BookingEndpointTest {
@@ -22,17 +24,22 @@ public class BookingEndpointTest {
 	
 	@Mock
 	private BookingService mockService;
-	
-	
+
+
 	@Test
-	public void testGetAllBookings() {
+	public void testGetAllSeats() {
 		Mockito.when(mockService.getAllBookings()).thenReturn(MOCKSTRING);
-		String bookingString = booking.get
+		String bookingString = booking.getBookingAsJson();
+		Mockito.verify(mockService).getAllBookings();
+		Assert.assertEquals(MOCKSTRING, bookingString);
 	}
 
 	@Test
-	public void testGetBookingByID() {
-		fail("Not yet implemented");
+	public void testGetSeatById() {
+		Mockito.when(mockService.getBookingByID(1)).thenReturn(MOCKSTRING);
+		String bookingString = booking.getBookingAsJson(1, MOCKSTRING);
+		Mockito.verify(mockService).getBookingById(1, MOCKSTRING);
+		Assert.assertEquals(MOCKSTRING, bookingString);
 	}
 
 }
