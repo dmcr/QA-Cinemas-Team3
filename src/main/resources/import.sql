2874809b849c55aa46c2cff98d8be2b3b256ec49
stephenCasey92 <stephen.casey92@gmail.com>
2017-02-23 15:11:59 +0000

    Dummy Seat Data x 4

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,12 @@
 insert into BookStore (id, author, title, genre, yearPublished) values (0, 'Stephen King', 'IT', 'Horror', '1988') 
+insert into cin_seat(seat_number, seat_row, seat_type_id, seat_screen_id) values
+(1, "A", 2, 1);
+
+insert into cin_seat(seat_number, seat_row, seat_type_id, seat_screen_id) values
+(10, "AA", 1, 10);
+
+insert into cin_seat(seat_number, seat_row, seat_type_id, seat_screen_id) values
+(1, "xx", 1, 3);
+
+insert into cin_seat(seat_number, seat_row, seat_type_id, seat_screen_id) values
+(11, "A", 2, 1);
