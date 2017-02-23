67a79f888b57e07141b73f519be2f7197fbc7d0a
Duncan McRobert <dmcrobert@live.co.uk>
2017-02-23 16:02:58 +0000

    Added cin_ticket insert

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -15,3 +15,6 @@ INSERT INTO cin_screen (screen_type_id,cin_screen_capacity) VALUES ('2','100');
 
 insert into cin_seat_type (type_name, type_price) values ('Standard', '10.00')
 insert into cin_seat_type (type_name, type_price) values ('Premier', '15.00')
+
+INSERT INTO cin_ticket(ticket_showing_id, tocket_booking_id, ticket_type, ticket_seat_id) VALUES (1, 1, 'adult', 1);
+
