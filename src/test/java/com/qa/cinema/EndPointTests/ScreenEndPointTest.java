71980d6ae4435a6ceba3d323f5912cd786821d9a
Duncan McRobert <dmcrobert@live.co.uk>
2017-03-01 11:51:26 +0000

    ScreenEndPointTest
    Updated screen end point test with expected json, ran tests

--- src/test/java/com/qa/cinema/EndPointTests/ScreenEndPointTest.java
+++ src/test/java/com/qa/cinema/EndPointTests/ScreenEndPointTest.java
@@ -17,24 +17,29 @@ public class ScreenEndPointTest {
 	@InjectMocks
 	private ScreenEndPoint subject;
 
-	private static final String MOCKSTRING = "[{\"screen_id\": 1,\"cin_screen_capacity\": 200,\"screen_type_id\": 14, \"seats\" : [],\"showings\":[] }]";
+	private static final String MOCKSTRINGALL = "[{\"screenId\": 1,\"cinScreenCapacity\": 200, \"seats\" : ["
+			+ "{\"seatId\": 1, \"number\" 1, \"row\": \"A\", \"seatType\": {\"typeId\", \"name\": \"standard\", \"price\": 10}, \"screenId\": 1 }],"
+			+ "\"showings\":[{\"showingId\": 1, \"startTime\": \"Jun 17, 0018 12:10:08 PM\", \"screenId\": 1, \"filmId\": 1 }]}]";
+	private static final String MOCKSTRING = "{\"screenId\": 1,\"cinScreenCapacity\": 200, \"seats\" : ["
+			+ "{\"seatId\": 1, \"number\" 1, \"row\": \"A\", \"seatType\": {\"typeId\", \"name\": \"standard\", \"price\": 10}, \"screenId\": 1 }],"
+			+ "\"showings\":[{\"showingId\": 1, \"startTime\": \"Jun 17, 0018 12:10:08 PM\", \"screenId\": 1, \"filmId\": 1 }]}";
 
 	@Mock
 	private ScreenService mockService;
 
 	@Test
 	public void testGetAllScreensAreReturnedCorrectly() {
-		Mockito.when(mockService.getAllScreens()).thenReturn(MOCKSTRING);
+		Mockito.when(mockService.getAllScreens()).thenReturn(MOCKSTRINGALL);
 		String screenString = subject.getScreenAsJson();
 		Mockito.verify(mockService).getAllScreens();
-		Assert.assertEquals(MOCKSTRING, screenString);
+		Assert.assertEquals(MOCKSTRINGALL, screenString);
 	}
 	
 	@Test
 	public void testGetScreentypebyIdreturnedCorrectly() {
-		Mockito.when(mockService.getScreentypebyId(12l)).thenReturn(MOCKSTRING);
-		String screenString = subject.getScreenbyIDAsJson(12l);
-		Mockito.verify(mockService).getScreentypebyId(12l);
+		Mockito.when(mockService.getScreentypebyId(1l)).thenReturn(MOCKSTRING);
+		String screenString = subject.getScreenbyIDAsJson(1l);
+		Mockito.verify(mockService).getScreentypebyId(1l);
 		Assert.assertEquals(MOCKSTRING, screenString);
 	}
 
