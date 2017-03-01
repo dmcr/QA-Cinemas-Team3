5e45ebc9a5074344f772acd39c80dd390dc5a55c
Duncan McRobert <dmcrobert@live.co.uk>
2017-03-01 12:00:10 +0000

    Updated tests with correct expected json examples

--- src/test/java/com/qa/cinema/EndPointTests/SeatEndPointTest.java
+++ src/test/java/com/qa/cinema/EndPointTests/SeatEndPointTest.java
@@ -19,29 +19,29 @@ public class SeatEndPointTest {
 	@InjectMocks
 	private SeatEndPoint subject;
 
-	private static final String MOCKSTRING = "[{\"id\": 1,\"Number\": 1,\"Row\": \"A\",\"seatType\": {},\"Screen\": {}}]";
+	private static final String MOCKSTRINGALL = "[{\"seatId\": 1,\"number\": 1,\"row\": \"A\",\"seatType\": {"
+			+ "\"typeId\": 1, \"name\": \"STANDARD\", \"price\": 10},\"screenId\": 1}]";
 	
-	private static final String MOCKSTRING1 = "{\"id\": 1,\"Number\": 1,\"Row\": \"A\",\"seatType\": {},\"Screen\": {}}";
-
-	
+	private static final String MOCKSTRING = "{\"seatId\": 1,\"number\": 1,\"row\": \"A\",\"seatType\": {"
+			+ "\"typeId\": 1, \"name\": \"STANDARD\", \"price\": 10},\"screenId\": 1}";
 
 	@Mock
 	private DBSeatService mockService;
 
 	@Test
 	public void testGetAllSeats() {
-		Mockito.when(mockService.getAllSeats()).thenReturn(MOCKSTRING);
+		Mockito.when(mockService.getAllSeats()).thenReturn(MOCKSTRINGALL);
 		String seatString = subject.getAllSeatAsJson();
 		Mockito.verify(mockService).getAllSeats();
-		Assert.assertEquals(MOCKSTRING, seatString);
+		Assert.assertEquals(MOCKSTRINGALL, seatString);
 	}
 
 	@Test
 	public void testGetSeatById() {
-		Mockito.when(mockService.getSeatById(1L)).thenReturn(MOCKSTRING1);
+		Mockito.when(mockService.getSeatById(1L)).thenReturn(MOCKSTRING);
 		String seatString = subject.getSeatAsJson(1L);
 		Mockito.verify(mockService).getSeatById(1L);
-		Assert.assertEquals(MOCKSTRING1, seatString);
+		Assert.assertEquals(MOCKSTRING, seatString);
 	}
 
 }
