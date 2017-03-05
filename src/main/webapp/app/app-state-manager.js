f382ccca0ca7995c0ab905bfc5f2f307347ac587
dmcr <dm393@kent.ac.uk>
2017-03-05 15:18:26 +0000

    Renamed folders to conform, updated app state manager with paths

--- src/main/webapp/app/app-state-manager.js
+++ src/main/webapp/app/app-state-manager.js
@@ -9,12 +9,12 @@
         $stateProvider.state("dashboard", {
             url: "/dashboard",
             templateUrl: "app/feature/dashboard/dashboard-partial.html"
-        }).state("films", {
-            url: "/films",
-            templateUrl: "app/feature/films/films.html"
-        }).state("/films", {
-            url: "/films",
-            templateUrl: "app/feature/bookings/get-booking-partial.html"
+        }).state("film", {
+            url: "/film",
+            templateUrl: "app/feature/film/films.html"
+        }).state("/film", {
+            url: "/film",
+            templateUrl: "app/feature/booking/get-booking-partial.html"
         })
     });
 }());
