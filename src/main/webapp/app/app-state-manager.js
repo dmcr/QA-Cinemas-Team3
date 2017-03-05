46875c83260fb28010e777103a0edfb4f49056f3
dmcr <dm393@kent.ac.uk>
2017-03-05 17:18:48 +0000

    Implemented get and add film page partials and service/controllers
    Working implementation, on addfilm gives json sysntax error but it adds
    correctly.  ?

--- src/main/webapp/app/app-state-manager.js
+++ src/main/webapp/app/app-state-manager.js
@@ -11,7 +11,10 @@
             templateUrl: "app/feature/dashboard/dashboard-partial.html"
         }).state("getfilm", {
             url: "/getfilm",
-            templateUrl: "app/feature/film/films.html"
+            templateUrl: "app/feature/film/get/get-film-partial.html"
+        }).state("addfilm", {
+            url: "/addfilm",
+            templateUrl: "app/feature/film/add/add-film-partial.html"
         }).state("getbooking", {
             url: "/getbooking",
             templateUrl: "app/feature/booking/get/get-booking-partial.html"
