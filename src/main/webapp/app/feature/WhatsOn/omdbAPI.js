2e0cc42e71be873e55e96b442ef4be3e5a0b16a6
Doods21 <chris.doodson@live.com>
2017-03-06 17:10:33 +0000

    whats on commit

--- src/main/webapp/app/feature/WhatsOn/omdbAPI.js
+++ src/main/webapp/app/feature/WhatsOn/omdbAPI.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp', [])
+angular.module('app', [])
     .controller('MovieController', function ($scope, $http) {
         $scope.$watch('search', function () {
             fetch();
