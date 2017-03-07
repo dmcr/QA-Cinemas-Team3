88465644b8301c0892e27ed82b34c40b8c602f5d
MarkBeresford <mberesford301@gmail.com>
2017-03-07 12:18:54 +0000

    Mark's changes.
                        Checking this works!

--- src/main/webapp/app/feature/film/get/js/app.js
+++ src/main/webapp/app/feature/film/get/js/app.js
@@ -6,7 +6,7 @@ angular.module('myApp', [])
       fetch();
     });
 
-    $scope.search = "Rogue One";
+    $scope.search = "Sherlock Holmes";
 
     function fetch(){
       $http.get("http://www.omdbapi.com/?t=" + $scope.search + "&tomatoes=true&plot=full")
@@ -14,13 +14,5 @@ angular.module('myApp', [])
 
       $http.get("http://www.omdbapi.com/?s=" + $scope.search)
       .then(function(response){ $scope.related = response.data; });
-    }
-
-    $scope.update = function(movie){
-      $scope.search = movie.Title;
-    };
-
-    $scope.select = function(){
-      this.setSelectionRange(0, this.value.length);
     }
   });
