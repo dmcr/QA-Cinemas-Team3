4fa40d32b5e88958741a59ef4aa9b15cced4591e
Doods21 <chris.doodson@live.com>
2017-03-07 16:15:08 +0000

    2 weeks wasted

---
+++ src/main/webapp/app/feature/WhatsOn/scripts/dim-background.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('.actionBox')
+        .mouseenter(function () {
+            $(this).dimBackground({
+                darkness: 0.7  // 0: no dimming, 1: completely black
+            });
+        })
+        .mouseleave(function () {
+            $(this).undim();
+        });
+});
