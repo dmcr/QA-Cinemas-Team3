db2e2ed55928ad1e74286f0e333a386fb741bca7
Doods21 <chris.doodson@live.com>
2017-03-08 15:04:07 +0000

    screen

--- src/main/webapp/app/feature/screen/add/add-screen-controller.js
+++ src/main/webapp/app/feature/screen/add/add-screen-controller.js
@@ -6,11 +6,13 @@
         vm.addScreen = function(screenToAdd) {
             screenDal.saveScreen(screenToAdd).then(function (results) {
                 vm.screenAddMessage  = results;
+                $state.go('cms.managescreens', {}, { reload: true });
             }, function (error) {
                 vm.error = true;
                 vm.errorMessage = error;
+                $state.go('cms.managescreens', {}, { reload: true });
             });
-            $state.go('cms.managescreens', {}, { reload: true });
+
         };
     };
 
