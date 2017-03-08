badb8eb834c6ade50c8c9edc07669881effa572c
MarkBeresford <mberesford301@gmail.com>
2017-03-08 10:41:04 +0000

    Mark's changes.
                        Checking this works!

--- src/main/webapp/app/feature/showing/get/get-showing-controller.js
+++ src/main/webapp/app/feature/showing/get/get-showing-controller.js
@@ -15,22 +15,6 @@
 
     };
 
-// var GetShowingTimeController = function (showingDal, $stateParams) {
-//     var vm = this;
-//
-//    app.controler('getShowingController', function Parsevariables($scope) {
-//         showingDal.getShowingStartTime($stateParams.showingId).then(function (result) {
-//             vm.showing = result;
-//         }, function(error) {
-//             vm.error = true;
-//             vm.errorMessage = error;
-//         });
-//     })
-//
-// }
-
-
-
     angular.module('cinemaApp').controller('getShowingController', ['showingDal', '$stateParams', GetShowingController]);
     // angular.module('cinemaApp').controller('GetShowingTimeController', ['showingDal', '$stateParams', GetShowingTimeController]);
 }());
