0261072fcc6f6d9b4dc6e7fe7d0eecb283d9e27e
Kimberrley <k_mcgill@hotmail.co.uk>
2017-03-09 09:14:48 +0000

    QC-Finish-Contact-Form-Updated
    Views split properly - STILL NEEDS CSS'D

--- src/main/webapp/app/app-state-manager.js
+++ src/main/webapp/app/app-state-manager.js
@@ -2,50 +2,95 @@
 
 (function () {
 
-    angular.module('cinemaApp').config(function ($stateProvider) {
+    angular.module('cinemaApp').config(function ($stateProvider, $urlRouterProvider) {
 
-        $stateProvider.state("getfilm", {
-            url: "/getfilm",
+        $urlRouterProvider.otherwise("/dashboard");
+
+        $stateProvider.state("dashboard", {
+            url: "/dashboard",
+            templateUrl: "app/feature/dashboard/dashboard-partial.html"
+        })
+            .state("getfilms", {
+            url: "/getfilms",
+            templateUrl: "app/feature/film/get/get-films-partial.html"
+        })
+            .state("getfilm", {
+            url: "/getfilm/{filmId:[0-9]}",
             templateUrl: "app/feature/film/get/get-film-partial.html"
-        }).state("addfilm", {
+        })
+            .state("addfilm", {
             url: "/addfilm",
             templateUrl: "app/feature/film/add/add-film-partial.html"
-        }).state("getbooking", {
-            url: "/getbooking",
+        })
+            .state("filminfo", {
+            url: "/filminfo",
+            templateUrl: "app/feature/film/get/partials/main-info.html"
+        })
+            .state("getbookings", {
+            url: "/getbookings",
+            templateUrl: "app/feature/booking/get/get-bookings-partial.html"
+        })
+            .state("getbooking", {
+            url: "/getbooking/{bookingId:[0-9]}",
             templateUrl: "app/feature/booking/get/get-booking-partial.html"
-        }).state("addbooking", {
+        })
+            .state("addbooking", {
         	url: "/addbooking",
         	templateUrl: "app/feature/booking/add/add-booking-partial.html"
-        }).state("addshowing", {
+        })
+            .state("addshowing", {
         	url: "/addshowing",
         	templateUrl: "app/feature/showing/add/add-showing-partial.html"
-        }).state("getshowing", {
-        	url: "/getshowing",
+        })
+            .state("getshowings", {
+        	url: "/getshowings",
+        	templateUrl: "app/feature/showing/get/get-showings-partial.html"
+        })
+            .state("getshowing", {
+        	url: "/getshowing/{showingId:[0-9]}",
         	templateUrl: "app/feature/showing/get/get-showing-partial.html"
-        }).state("addticket", {
+        })
+            .state("addticket", {
         	url: "/addticket",
         	templateUrl: "app/feature/ticket/add/add-ticket-partial.html"
-        }).state("getticket", {
+        })
+            .state("getticket", {
         	url: "/getticket",
         	templateUrl: "app/feature/ticket/get/get-ticket-partial.html"
-        }).state("addscreen", {
+        })
+            .state("addscreen", {
         	url: "/addscreen",
         	templateUrl: "app/feature/screen/add/add-screen-partial.html"
-        }).state("getscreen", {
+        })
+            .state("getscreen", {
         	url: "/getscreen",
         	templateUrl: "app/feature/screen/get/get-screen-partial.html"
-        }).state("addseat", {
+        })
+            .state("addseat", {
         	url: "/addseat",
         	templateUrl: "app/feature/seat/add/add-seat-partial.html"
-        }).state("getseat", {
+        })
+            .state("getseat", {
         	url: "/getseat",
         	templateUrl: "app/feature/seat/get/get-seat-partial.html"
-        }).state("addseattype", {
+        })
+            .state("addseattype", {
         	url: "/addseattype",
         	templateUrl: "app/feature/seatType/add/add-seatType-partial.html"
-        }).state("getseattype", {
+        })
+            .state("getseattype", {
         	url: "/getseattype",
         	templateUrl: "app/feature/seatType/get/get-seatType-partial.html"
+        })
+            .state("getfilminfo", {
+            url: "/getfilminfo/{filmId:[0-9]}",
+            templateUrl: "app/feature/showing/get/get-showing-partial.html"
+        })
+            .state("BookingTest", {
+            url: "/BookingTest",
+            templateUrl: "app/feature/showing/get/BookingTest.html",
+            params: {starttime: null}
+
         })
     });
 }());
